0500b94fd1dc169c567d4712d628a83b64750760
Johnny Starr <johnnystarr@gmail.com>
2021-11-18 09:29:42 -0600

    fixing colors

--- app/main.sh
+++ app/main.sh
@@ -1,3 +1,1 @@
 logo
-
-say "_r_pi_b_zza _g_time_x_"
--- lib/colorize.sh
+++ lib/colorize.sh
@@ -6,16 +6,16 @@ colorize() {
     colors[_g_]="\033[0;32m"
     colors[_y_]="\033[0;33m"
     colors[_b_]="\033[0;34m"
-    colors[_c_]="\033[0;35m"
-    colors[_m_]="\033[0;36m"
+    colors[_m_]="\033[0;35m"
+    colors[_c_]="\033[0;36m"
     colors[_x_]="\033[0;37m"
     colors[_R_]="\033[1;31m"
     colors[_G_]="\033[1;32m"
     colors[_Y_]="\033[1;33m"
     colors[_B_]="\033[1;34m"
-    colors[_C_]="\033[1;35m"
-    colors[_M_]="\033[1;36m"
-    colors[_X_]="\033[1;37m"
+    colors[_M_]="\033[1;35m"
+    colors[_C_]="\033[1;36m"
+    colors[_W_]="\033[1;37m"
 
     for k in "${!colors[@]}"; do
         escaped_value=$(printf '%s\n' "${colors[$k]}" | sed -e 's/[\/&]/\\&/g')
